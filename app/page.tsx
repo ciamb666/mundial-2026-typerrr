39633589a76bbc982f54ffeece97a2b7f8ffddea
domci <pizdanadglowa1990@o2.pl>
2026-06-08 22:24:33 +0200

    Pierwsza wersja aplikacji

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Typowanie Mundial 2026
+      </h1>
+
+      <p className="mt-4 text-xl">
+        Witaj w naszej lidze typerów!
+      </p>
+    </main>
+  );
+}
